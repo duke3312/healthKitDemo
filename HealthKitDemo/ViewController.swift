d2a4988ac8840aa01501863d439c0340e8286e32
DuWenQing <duke3312@163.com>
2018-09-04 15:34:31 +0800

    Initial Commit

---
+++ HealthKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKitDemo
+//
+//  Created by 杜文庆 on 2018/9/4.
+//  Copyright © 2018年 FanXing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
